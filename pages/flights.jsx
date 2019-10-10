c346b685f26c9790448e3bf70ffa8e081524755c
Facundo Palombo <facundo.palombo@gmail.com>
2019-10-10 18:55:44 -0300

    Mejorando la lógica del componente flights

--- pages/flights.jsx
+++ pages/flights.jsx
@@ -7,6 +7,8 @@ import { formatPrice, formatFlyTime } from '../utils/formatters'
 import Layout from '../components/Layout';
 
 function Flights(props) {
+  console.log(props)
+  const {flights} = props
   return (
     <Layout>
       <style jsx>
@@ -19,8 +21,6 @@ function Flights(props) {
           }
           ul {
             display: grid;
-            grid-template-columns: repeat(3, minmax(300px, 1fr));
-            grid-template-rows: repeat(auto-fill, minmax(300px, 1fr));
             grid-gap: 30px;
             margin-top: 30px;
             padding: 0 30px;
@@ -28,7 +28,6 @@ function Flights(props) {
           a, a:hover, a:active{
             text-decoration: none;
             color: #222;
-            cursor: pointer
 
           }
           a:active {
@@ -41,7 +40,7 @@ function Flights(props) {
           <h2>Choose your outbound flight to Mendoza</h2>
           <section>
             <ul>
-              {props.mdz
+              {flights
                 .filter(item => item.origin === 'MDZ')
                 .map((item, key) => {
                   const { origin, destination, flightNo, arrivalDate, departureDate, currency } = item;
@@ -55,16 +54,16 @@ function Flights(props) {
                         }}
                         prefetch
                       >
-                        <a>
-                          <Card
-                            name={`Flight N° ${flightNo}`}
-                            code={`${origin}.${destination}-${flightNo}`}
-                            origin={origin}
-                            destination={destination}
-                          >
-                          <h4>{formatFlyTime(arrivalDate,departureDate)}de vuelo</h4>
-                          <h4>{formatPrice(item.fares[0].prices.afterTax, currency)}</h4></Card>
-                        </a>
+                      <a>
+                        <Card
+                          name={`Flight N° ${flightNo}`}
+                          code={`${origin}.${destination}-${flightNo}`}
+                          origin={origin}
+                          destination={destination}
+                        >
+                        <h4>{formatFlyTime(arrivalDate,departureDate)}de vuelo</h4>
+                        <h4>{formatPrice(item.fares[0].prices.afterTax, currency)}</h4></Card>
+                      </a>
                       </Link>
                     </li>
                   );
@@ -76,7 +75,7 @@ function Flights(props) {
           <h2>Choose your inbound flight to Buenos Aires</h2>
           <section>
             <ul>
-              {props.cor
+              {flights
                 .filter(item => item.destination === 'EPA')
                 .map((item, key) => {
                   const { origin, destination, flightNo, arrivalDate, departureDate, currency } = item;
@@ -99,35 +98,6 @@ function Flights(props) {
                           >
                           <h4>{formatFlyTime(arrivalDate,departureDate)}de vuelo</h4>
                           <h4>{formatPrice(item.fares[0].prices.afterTax, currency)}</h4></Card>
-                        </a>
-                      </Link>
-                    </li>
-                  );
-                })}
-              {props.mdz
-                .filter(item => item.destination === 'EPA')
-                .map((item, key) => {
-                  const { origin, destination, flightNo, arrivalDate, departureDate, currency } = item;
-                  return (
-                    <li key={key}>
-                      <Link
-                        route="flight"
-                        params={{
-                          origin,
-                          destination
-                        }}
-                        prefetch
-                      >
-                        <a>
-                          <Card
-                            name={`Flight N° ${flightNo}`}
-                            code={`${origin}.${destination}-${flightNo}`}
-                            origin={origin}
-                            destination={destination}
-                          >
-                          <h4>{formatFlyTime(arrivalDate,departureDate)}de vuelo</h4>
-                          <h4>{formatPrice(item.fares[0].prices.afterTax, currency)}</h4>
-                          </Card>
                         </a>
                       </Link>
                     </li>
@@ -148,7 +118,7 @@ Flights.getInitialProps = props => {
   const {
     default: { flights: mdz }
   } = mdzData;
-  const flights = { cor, mdz };
+  const flights = {flights: cor.concat(mdz)}
   return flights;
 };
 
